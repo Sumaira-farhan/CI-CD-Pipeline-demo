13960ef6ac3ed12ed2648bb67b166368e3d5feb9
Sumaira-farhan <sumairafattah7@gmail.com>
2026-05-31 12:04:58 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+model = pickle.load(open("model.pkl", "rb"))
+
+st.title("Student Pass Prediction App")
+
+hours = st.number_input("Enter study hours")
+
+if st.button("Predict"):
+    prediction = model.predict([[hours]])
+
+    if prediction[0] == 1:
+        st.success("Student will PASS 🎉")
+    else:
+        st.error("Student may FAIL ❌")
